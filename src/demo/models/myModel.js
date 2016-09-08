27f8ba8037cb98d3547f91c0d52222d3dcd8bc43
colxi <colxi.kl@gmail.com>
2016-09-08 19:32:46 +0200

    Demo improve

--- src/demo/models/myModel.js
+++ src/demo/models/myModel.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
 
 let myModel = {
-    about : 'This is an example model where can be appreciated model nesting, inheritage',
-    consideration : ' Rivets-import is EXPERIMENTAL. Do not forget it.',
-    warn : function(){ alert('WARN: This library is Not ready for  production enviroments. You are warned!!'); },
+    about : 'This is an example model where can be appreciated the lazy load of model and view , and the model nesting, and scopes inheritage',
+    consideration : 'Rivets-import is EXPERIMENTAL. Do not forget it.',
+    warn : function(){ alert('WARN: This library is Not ready for  production enviroments. You are warned!(again)'); },
     toggleDebug : function(){ rivets.configure_importer({ debug : !rivets.imports.__debug__ }); },
     author : {
         name : 'colxi',
